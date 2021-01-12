e96262a43bc249e6a1225320a00de6b68f889e97
Yosh11 <isok.1997@mail.ru>
2021-01-12 16:32:01 +0300

    feat migrate

--- myfirst/myfirst/apps/testsite/models.py
+++ myfirst/myfirst/apps/testsite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(("Title site"), max_length=100)
+    article_text = models.TextField(("Site text"))
+    pub_date = models.DateTimeField(("publication date"))
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField(("Name author"), max_length=50)
+    comm_text = models.CharField(("Comment text"), max_length=100)
+
