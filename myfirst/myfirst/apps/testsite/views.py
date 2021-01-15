ae11fd5e92c16cbe34e0bee8ff3071b5f288ec0d
Yosh11 <isok.1997@mail.ru>
2021-01-15 23:17:37 +0300

    feat front

--- myfirst/myfirst/apps/testsite/views.py
+++ myfirst/myfirst/apps/testsite/views.py
@@ -1,4 +1,29 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello world!!!")
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, "testsite/list.html", {'latest_articles_list' : latest_articles_list})
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id= article_id)
+    except:
+        raise Http404("Page not found")
+    
+    latest_comment_list = a.comment_set.order_by('-id')[:10]
+    
+    return render(request, 'testsite/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id= article_id)
+    except:
+        raise Http404("Page not found")
+    
+    a.comment_set.create(author_name = request.POST['name'], comm_text = request.POST['text'])
+
+    return HttpResponseRedirect( reverse('testsite:detail', args=(a.id,)))
